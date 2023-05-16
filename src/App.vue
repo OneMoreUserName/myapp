fab9f27c342263348bde23b5b5cc06a729f8f546
OneMoreUserName <polulika@mail.ru>
2023-05-16 15:07:46 +0300

    bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div>HelloWorld</div>
+  <Upcliker/>
 </template>
+
+<script setup lang="ts">
+import Upcliker from './components/Upcliker.vue';
+</script>
 
 <style scoped>
 </style>
